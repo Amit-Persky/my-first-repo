a9e9ba060842f2a537c5ba3525e996d8c569cb64
A.P. <159085398+Amit-Persky@users.noreply.github.com>
2024-11-13 01:50:36 +0200

    Update main.py
    change

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Student import Student
 from Employee import Employee
 
 student = Student("Mike", 22, "Engineering", 3, 99)
-print("This is from programmer 1, 123")
+print("This is from programmer 1, 123 hi how are you")
 # student.foo()
 print("This is from programmer 2")
 
