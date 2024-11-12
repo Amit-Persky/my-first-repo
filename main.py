096e605e9ef441e0da3ce4d666e8f5bd3651c24f
Amit P <amitnicetomeet@gmail.com>
2024-11-13 01:06:27 +0200

    fixed bug missing avg score

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Student import Student
 from Employee import Employee
 
-student = Student("Mike", 22, "Engineering", 3)
+student = Student("Mike", 22, "Engineering", 3, 99)
 # student.foo()
 
 employee = Employee("John", 40, "Software Engineer", 45000)
