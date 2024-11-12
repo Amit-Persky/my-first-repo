f5e062259e15ccddb38201577a46a985a5bc4998
Amit P <amitnicetomeet@gmail.com>
2024-11-13 01:48:13 +0200

    adding 123 to programmer 1 print

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Student import Student
 from Employee import Employee
 
 student = Student("Mike", 22, "Engineering", 3, 99)
-print("This is from programmer 1 hi brother")
+print("This is from programmer 1, 123")
 # student.foo()
 print("This is from programmer 2")
 
